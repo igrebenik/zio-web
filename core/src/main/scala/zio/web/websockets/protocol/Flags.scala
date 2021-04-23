bbe0531e865be90984ad22c27293e8adc851c792
Ivan Grebenik <vanya.grebenik@gmail.com>
2021-04-24 02:05:47 +0300

    Define codec for Flags

--- core/src/main/scala/zio/web/websockets/protocol/Flags.scala
+++ core/src/main/scala/zio/web/websockets/protocol/Flags.scala
@@ -1,8 +1,16 @@
 package zio.web.websockets.protocol
 
+import zio.web.websockets.codec.{ BitChunk, FrameCodec }
+
 final case class Flags(fin: Boolean, rsv1: Boolean = false, rsv2: Boolean = false, rsv3: Boolean = false)
 
 object Flags {
   val last: Flags     = Flags(true)
   val continue: Flags = Flags(false)
+
+  val codec: FrameCodec[Flags] =
+    FrameCodec[Flags](
+      flags => BitChunk.bits(flags.fin, flags.rsv1, flags.rsv2, flags.rsv3),
+      bits => Flags(bits.get(0), bits.get(1), bits.get(2), bits.get(3))
+    )
 }
